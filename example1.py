36210e5559f2cba3eecf988667320cbae00ac7cc
jerrywuhi <wu0952686160@gmail.com>
2026-03-17 11:52:48 +0800

    網頁設計

---
+++ example1.py
@@ -0,0 +1,10 @@
+def process(x):
+    x= x.split(" ")
+    school = x[0].replace("我是", "")
+    print(f"學校:{school}\n姓名:{x[2]}")
+
+if __name__ == "__main__":
+    name = "我是靜宜大學 資管二A 吳冠頡"
+    process(name)
+
+    
---
+++ example2.py
@@ -0,0 +1,4 @@
+from example1 import process
+
+name = "我是靜宜大學 資管二A 吳冠頡"
+process(name)
